5ee387cb0188aa469dbd4b94a821f8db00ea419e
yeyi58095 <141631770+yeyi58095@users.noreply.github.com>
2025-07-10 14:28:26 +0800

    character running

--- Youtube24_MultiScenes/Assets/Scripts/MakeCharacterMove/WaveControl.cs
+++ Youtube24_MultiScenes/Assets/Scripts/MakeCharacterMove/WaveControl.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class WaveControl : MonoBehaviour {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() {
 
+    private Animator animator;
+    void Start() {
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -11,7 +13,23 @@ public class WaveControl : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.F)) {
             // Trigger the wave animation
             GetComponent<Animator>().SetTrigger("wave");
-
+            // or animator.SetTrigger("wave");
         }
+
+
+        // for rotation the direction of the character
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(horizontal, 0, vertical);
+       
+
+        if(dir != Vector3.zero) {
+             transform.rotation = Quaternion.LookRotation(dir);
+            animator.SetBool("IsRun", true);
+            transform.Translate(Vector3.forward * Time.deltaTime * 2f);    
+        } else {
+            animator.SetBool("IsRun", false);
+        }
+
     }
 }
