98448957a864d61f7e1ba0b9bd13b33a89309c59
yeyi58095 <141631770+yeyi58095@users.noreply.github.com>
2025-07-10 14:58:22 +0800

    learning the events and curves in the animator

--- Youtube24_MultiScenes/Assets/Scripts/MakeCharacterMove/WaveControl.cs
+++ Youtube24_MultiScenes/Assets/Scripts/MakeCharacterMove/WaveControl.cs
@@ -21,15 +21,24 @@ public class WaveControl : MonoBehaviour {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector3 dir = new Vector3(horizontal, 0, vertical);
-       
 
-        if(dir != Vector3.zero) {
-             transform.rotation = Quaternion.LookRotation(dir);
+
+        if (dir != Vector3.zero) {
+            transform.rotation = Quaternion.LookRotation(dir);
             animator.SetBool("IsRun", true);
-            transform.Translate(Vector3.forward * Time.deltaTime * 2f);    
+            transform.Translate(Vector3.forward * Time.deltaTime * 2f);
         } else {
             animator.SetBool("IsRun", false);
         }
 
+       // Debug.Log("Test: " + animator.GetFloat("Test"));
+    }
+
+    void LeftFoot() {
+        Debug.Log("Left Foot");
+    }
+
+    void RightFoot() {
+        Debug.Log("Right Foot");
     }
 }
